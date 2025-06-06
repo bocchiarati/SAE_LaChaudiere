aad0092b0e2b08c3a80f3e2c3cc47ac42f27274b
bocchiarati <88000sofien@gmail.com>
2025-06-06 10:17:54 +0200

    Add API resource loader with configurable entrypoint

---
+++ web/js/lib/api_loader.js
@@ -0,0 +1,25 @@
+import { entrypoint } from "../conf.js";
+
+/**
+ * Charge une ressource à partir d'une URL donnée.
+ *
+ * @param {string} url - L'URL de la ressource à charger.
+ * @returns {Promise<Object>} Une promesse qui se résout avec les données de la ressource.
+ */
+async function loadRessource(url) {
+    return (await fetch(entrypoint + url, { credentials: 'include' })).json();
+}
+
+/**
+ * Récupère une ressource à partir d'une URL donnée.
+ *
+ * @param {string} url - L'URL de la ressource à récupérer.
+ * @returns {Promise<Object>} Une promesse qui se résout avec les données de la ressource.
+ */
+export async function getRessource(url) {
+    try {
+        return await loadRessource(url);
+    } catch (err) {
+        console.error("Erreur lors du chargement de la ressource :", err);
+    }
+}

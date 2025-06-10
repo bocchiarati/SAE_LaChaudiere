670019b5817249be134753d06ad1bf501add3513
bocchiarati <88000sofien@gmail.com>
2025-06-10 14:39:04 +0200

    ajout méthodes verification utilisateur + fix merge conflict

--- core/src/webui/providers/SessionAuthnProvider.php
+++ core/src/webui/providers/SessionAuthnProvider.php
@@ -5,6 +5,9 @@ namespace App\webui\providers;
 use App\application_core\application\auth\interfaces\AuthnServiceInterface;
 use App\application_core\domain\entities\User;
 use App\webui\providers\interfaces\AuthnProviderInterface;
+use Slim\Psr7\Response;
+use Slim\Psr7\Request;
+
 
 class SessionAuthnProvider implements AuthnProviderInterface {
     private AuthnServiceInterface $authnService;
@@ -29,5 +32,40 @@ class SessionAuthnProvider implements AuthnProviderInterface {
 
     public function signout(): void {
         session_unset();
+    }
+
+    public function verifyUser(Response $response, Request $request) : ?Response {
+        if(!isConnected()) {
+            $routeParser = RouteContext::fromReqsigninuest($request)->getRouteParser();
+            $url = $routeParser->urlFor('signin');
+            return $response
+                ->withHeader('Location', $url)
+                ->withStatus(302);
+        } else if(!isAdmin()) {
+            $routeParser = RouteContext::fromRequest($request)->getRouteParser();
+            $url = $routeParser->urlFor('unvalide_user');
+            return $response
+                ->withHeader('Location', $url)
+                ->withStatus(302);
+        } else {
+            return null;
+        }
+    }
+
+    private function isAdmin() : bool {
+        if($this->isConnected()) {
+            $user = $this->getSignedInUser();
+            if(in_array("admin", $user->roles)) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private function isConnected() : bool {
+        if($this->getSignedInUser() !== null) {
+            return true;    
+        }
     }
 }
--- core/src/webui/providers/interfaces/AuthnProviderInterface.php
+++ core/src/webui/providers/interfaces/AuthnProviderInterface.php
@@ -2,8 +2,12 @@
 
 namespace App\webui\providers\interfaces;
 
+use Slim\Psr7\Response;
+use Slim\Psr7\Request;
+
 interface AuthnProviderInterface {
     public function getSignedInUser(): ?array;
     public function signin($email, $mdp, $id);
     public function signout() : void;
+    public function verifyUser(Response $response, Request $request) : ?Response;
 }

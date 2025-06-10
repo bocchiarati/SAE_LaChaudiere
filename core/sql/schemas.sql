7b31ffb347333c64cca39f407c604a9c75853838
Hugo Bena <heno.drawing@gmail.com>
2025-06-10 11:43:04 +0200

    changement sql pour gérer les accents en bd

--- core/sql/schemas.sql
+++ core/sql/schemas.sql
@@ -1,19 +1,16 @@
--- Create the User table with VARCHAR id
 CREATE TABLE User (
                       id VARCHAR(50) PRIMARY KEY,
                       email VARCHAR(255) NOT NULL UNIQUE,
                       password VARCHAR(255) NOT NULL,
                       role INT NOT NULL
-);
+)DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Create the Category table
 CREATE TABLE Category (
                           id INT AUTO_INCREMENT PRIMARY KEY,
                           label VARCHAR(255) NOT NULL,
                           description TEXT
-);
+)DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Create the Event table
 CREATE TABLE Event (
                        id INT AUTO_INCREMENT PRIMARY KEY,
                        title VARCHAR(255) NOT NULL,
@@ -27,12 +24,11 @@ CREATE TABLE Event (
                        user_id VARCHAR(50),
                        FOREIGN KEY (category_id) REFERENCES Category(id),
                        FOREIGN KEY (user_id) REFERENCES User(id)
-);
+)DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Create the Image table
 CREATE TABLE Image (
                        id INT AUTO_INCREMENT PRIMARY KEY,
                        event_id INT,
                        url VARCHAR(255) NOT NULL,
                        FOREIGN KEY (event_id) REFERENCES Event(id)
-);
+)DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

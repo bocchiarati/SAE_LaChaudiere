53ce2cc2bdc2cfe403052a92034ea3c9b4760225
bocchiarati <88000sofien@gmail.com>
2025-06-12 11:04:34 +0200

    instanciation de la méthode de filtre des events en favoris

--- web/js/lib/calendar.js
+++ web/js/lib/calendar.js
@@ -1,6 +1,7 @@
 import {load_event} from "../views/event_ui.js";
 import {getRessource} from "./api_loader.js";
 import {category_filtre_action} from "../controller/category_filtre_action.js";
+import {filtre_favoris_action} from "../controller/favorites_action.js";
 
 export function initCalendar() {
 
@@ -20,6 +21,7 @@ export function initCalendar() {
     calendar.render();
     loadEvents("/api/events", calendar);
     category_filtre_action(calendar);
+    filtre_favoris_action(calendar);
 }
 
 export async function loadEvents(url, calendar) {

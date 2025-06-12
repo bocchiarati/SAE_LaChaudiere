8e3cb2fe8d4a8a656df8607d598d3d57adf6ae2f
bocchiarati <88000sofien@gmail.com>
2025-06-12 11:01:53 +0200

    ajout de la fonction de filtrage des event favoris dans le calendrier

--- web/js/controller/favorites_action.js
+++ web/js/controller/favorites_action.js
@@ -28,3 +28,34 @@ export function toggle_favorites_action(event_id) {
         btn.classList.toggle("text-gray-400", index !== -1); // redevient gris si retiré
     });
 }
+
+export function filtre_favoris_action(calendar){
+    const btn = document.getElementById("show_favoris");
+    if (!btn) return;
+    let toggleFiltre = false;
+    btn.addEventListener("click", function () {
+        let favoris = sessionStorage.getItem("favoris");
+        favoris = favoris ? JSON.parse(favoris) : [];
+        favoris = favoris
+            .filter(id => id !== null)
+            .map(id => Number(id));
+
+        for (const event of calendar.getEvents()) {
+            const currentClasses = event.classNames || [];
+            if(!toggleFiltre){
+                console.log(event.id);
+                console.log("------------------");
+                console.log(favoris);
+                console.log(favoris.includes(Number(event.id)));
+                if(!favoris.includes(Number(event.id))){
+                    event.setProp('classNames', [...currentClasses, 'hidden']);
+                }
+            }
+            else{
+                const cleaned = currentClasses.filter(cls => cls !== 'hidden');
+                event.setProp('classNames', cleaned);
+            }
+        }
+        toggleFiltre = !toggleFiltre;
+    })
+}

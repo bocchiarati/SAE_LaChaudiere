588ef7235c2fefd238b75c0cebad91ec5cd3e8c3
Hugo Bena <heno.drawing@gmail.com>
2025-06-12 14:10:16 +0200

    ajout de dépendances du projet

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => EventProvider()..fetchEvents(),
+      create: (context) => EventProvider(),
       child: MaterialApp(
         title: 'La Chaudière',
         theme: ThemeData(

64db26184d600905b03a8a10f091b5089602efbe
Hugo Bena <heno.drawing@gmail.com>
2025-06-13 10:45:58 +0200

    ajout d'un mode dark/lignt + génération des images pour le détail des évènements (main et theme_provider.dart)

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,25 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_liste/providers/event_provider.dart';
+import 'package:todo_liste/providers/theme_provider.dart';
 import 'package:todo_liste/screen/EventListScreen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+final ThemeData lightTheme = ThemeData.from(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.redAccent,
+    brightness: Brightness.light,
+  ),
+);
+
+final ThemeData darkTheme = ThemeData.from(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.redAccent,
+    brightness: Brightness.dark,
+  ),
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => EventProvider(),
-      child: MaterialApp(
-        title: 'La Chaudière',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        ),
-        home: const EventListScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => EventProvider()),
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+      ],
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            title: 'La Chaudière',
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            themeMode: themeProvider.themeMode,
+            home: const EventListScreen(),
+          );
+        },
       ),
     );
   }
